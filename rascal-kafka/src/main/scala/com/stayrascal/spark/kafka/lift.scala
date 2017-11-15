1176103fba1bedc1f36a22ceff7de11ace9754b3
Zhiping Wu <zpwu@thoughtworks.com>
2017-11-15 21:38:57 +0800

    reformat lift

--- rascal-kafka/src/main/scala/com/stayrascal/spark/kafka/lift.scala
+++ rascal-kafka/src/main/scala/com/stayrascal/spark/kafka/lift.scala
@@ -1,14 +1,16 @@
 package com.stayrascal.spark.kafka
 
-object liftModule extends App{
-  def add(x:Int)(y:Int):Int = x + y
+object liftModule extends App {
+  def add(x: Int)(y: Int): Int = x + y
 
-  def lift[A,B,C](f: A => B => C): Option[A] => Option[B] => Option[C] = {
+  def lift[A, B, C](f: A => B => C): Option[A] => Option[B] => Option[C] = {
     // o1 => o2 => o1.flatMap(x => o2.map(y => f(x)(y)))
-    o1 => o2 => for{
-      x <- o1
-      y <- o2
-    } yield f(x)(y)
+    o1 =>
+      o2 =>
+        for {
+          x <- o1
+          y <- o2
+        } yield f(x)(y)
   }
 
   val fadd = lift(add)

9c0f2a5f89f442fa6314aae41f8d0c17e829dcde
Zhiping Wu <zpwu@thoughtworks.com>
2017-12-24 19:13:28 +0800

    FP Scala: update applicative

--- rascal-data/src/main/scala/com/stayrascal/spark/example/fp/appllicative/Applicative.scala
+++ rascal-data/src/main/scala/com/stayrascal/spark/example/fp/appllicative/Applicative.scala
@@ -1,5 +1,7 @@
 package com.stayrascal.spark.example.fp.appllicative
 
+import com.stayrascal.spark.example.fp.monod.Monad
+
 trait Functor[F[_]] {
   def map[A, B](fa: F[A])(f: A => B): F[B]
 
@@ -14,7 +16,7 @@ trait Functor[F[_]] {
 trait Applicative[F[_]] extends Functor[F] {
   def unit[A](a: => A): F[A]
 
-  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_(_))
+  def apply[A, B](fab: F[A => B])(fa: F[A]): F[B] = map2(fab, fa)(_ (_))
 
   def map[A, B](fa: F[A])(f: A => B): F[B] = apply(unit(f))(fa)
 
@@ -30,9 +32,55 @@ trait Applicative[F[_]] extends Functor[F] {
 
   def replicateM[A](n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
 
-  def product[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+  def factor[A, B](fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
+
+  /*def product[G[_]](G: Applicative[G]): Applicative[({type f[x] = (F[x], G[x])})#f] = {
+    val self = this
+    new Applicative[({type f[x] = (F[x], G[x])})#f] {
+      override def unit[A](a: => A): (F[A], G[A]) = (self.unit(a), G.unit(a))
+
+      override def apply[A, B](fab: (F[A => B], G[A => B]))(fa: (F[A], G[A])): (F[B], G[B]) = (self.apply(fab._1)(fa._1), G.apply(fab._2)(fa._2))
+    }
+  }*/
+
+  /*def compose[G[_]](G: Applicative[G]): Applicative[({type f[x] = F[G[x]]})#f] = {
+    val self = this
+    new Applicative[({type f[x] = F[G[x]]})#f] {
+      override def unit[A](a: => A): F[G[A]] = self.unit(G.unit(a))
+
+      override def map2[A, B, C](fa: F[G[A]], fb: F[G[B]])(f: (A, B) => C): F[G[C]] = self.map2(fa, fb)(G.map2(_, _)(f))
+    }
+  }*/
 }
 
-object Applicative {
+object Monad {
+  def eitherMonad[E]: Monad[({type f[x] = Either[E, x]})#f] =
+    new Monad[({type f[x] = Either[E, x]})#f] {
+      override def unit[A](a: => A): Either[E, A] = Right(a)
 
+      override def flatMap[A, B](ma: Either[E, A])(f: A => Either[E, B]): Either[E, B] = ma match {
+        case Right(a) => f(a)
+        case Left(b) => Left(b)
+      }
+    }
+}
+
+sealed trait Validation[+E, +A]
+
+case class Failure[E](head: E, tail: Vector[E]) extends Validation[E, Nothing]
+
+case class Success[A](a: A) extends Validation[Nothing, A]
+
+object Applicative {
+  def validationApplicative[E]: Applicative[({type f[x] = Validation[E, x]})#f] =
+    new Applicative[({type f[x] = Validation[E, x]})#f] {
+      override def unit[A](a: => A): Validation[E, A] = Success(a)
+
+      override def map2[A, B, C](fa: Validation[E, A], fb: Validation[E, B])(f: (A, B) => C): Validation[E, C] = (fa, fb) match {
+        case (Success(a), Success(b)) => Success(f(a, b))
+        case (Failure(h1, t1), Failure(h2, t2)) => Failure(h1, t1 ++ Vector(h2) ++ t2)
+        case (e@Failure(_, _), _) => e
+        case (_, e@Failure(_, _)) => e
+      }
+    }
 }

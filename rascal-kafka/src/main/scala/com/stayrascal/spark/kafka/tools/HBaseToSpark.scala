2c4694760770413dfe91629cfd0b508c55212d3c
Zhiping Wu <zpwu@thoughtworks.com>
2017-12-30 23:49:00 +0800

    update hbase tool

--- rascal-kafka/src/main/scala/com/stayrascal/spark/kafka/tools/HBaseToSpark.scala
+++ rascal-kafka/src/main/scala/com/stayrascal/spark/kafka/tools/HBaseToSpark.scala
@@ -1,8 +1,9 @@
 package com.stayrascal.spark.kafka.tools
 
 import org.apache.hadoop.conf.Configuration
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.HTable
+import org.apache.hadoop.hbase.client.{ConnectionFactory, Get, Put, Table}
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.{HBaseConfiguration, TableName}
 
 import scala.collection.mutable
 
@@ -11,7 +12,7 @@ object HBaseToSpark {
   hadoopConf.addResource(getClass.getClassLoader.getResourceAsStream("core-site.xml"))
   hadoopConf.addResource(getClass.getClassLoader.getResourceAsStream("hdfs-site.xml"))
 
-  val table = new mutable.HashMap[String, HTable]()
+  val table = new mutable.HashMap[String, Table]()
   val conf = HBaseConfiguration.create(hadoopConf)
 
   def main(args: Array[String]): Unit = {
@@ -21,6 +22,39 @@ object HBaseToSpark {
     conf.addResource(getClass.getClassLoader.getResourceAsStream("hbase-policy.xml"))
     conf.addResource(getClass.getClassLoader.getResourceAsStream("hbase-site.xml"))
 
+    val connection = ConnectionFactory.createConnection(conf)
+
+    def getTable(tableName: String): Table = {
+      val tal = connection.getTable(TableName.valueOf(tableName))
+      table(tableName) = tal
+      tal
+    }
+
+    def getValue(tableName: String, rowKey: String, family: String, qualifiers: Array[String]): Array[(String, String)] = {
+      val tbl = getTable(tableName)
+      val row = tbl.get(new Get(Bytes.toBytes(rowKey)))
+      if (row != null && !row.isEmpty) {
+        qualifiers.map(qualifier => (tableName + "." + qualifier, Bytes.toString(row.getValue(Bytes.toBytes(family), Bytes.toBytes(qualifier)))))
+      } else {
+        qualifiers.map(qualifier => (tableName + "." + qualifier, "null"))
+      }
+    }
+
+    def putValue(tableName: String, rowKey: String, family: String, qualifierValues: Array[(String, String)]) = {
+      val tal = getTable(tableName)
+      val row = new Put(Bytes.toBytes(rowKey))
+
+      qualifierValues.map(qualifier => {
+        var column = qualifier._1
+        val value = qualifier._2
+        val tt = column.split("\\.")
+        if (tt.length == 2) column = tt(1)
+        if (!value.isEmpty) {
+          row.addColumn(Bytes.toBytes(family), Bytes.toBytes(column), Bytes.toBytes(value))
+        }
+      })
+      tal.put(row)
+    }
   }
 
 }
